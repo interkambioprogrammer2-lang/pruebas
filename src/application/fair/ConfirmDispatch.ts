a0341afb1c9a1bf7a94e2030a1f406d287ee3510
ghostnotfound404 <dbbastidas08@gmail.com>
2026-05-09 01:30:52 -0500

    feat: Update ConfirmDispatch to return Fair object on dispatch confirmation

--- src/application/fair/ConfirmDispatch.ts
+++ src/application/fair/ConfirmDispatch.ts
@@ -1,10 +1,12 @@
 import { IFairRepository } from '../../domain/repositories/IFairRepository';
+import { Fair } from '../../domain/entities/Fair';
 
 export class ConfirmDispatch {
   constructor(private fairRepo: IFairRepository) {}
 
-  async execute(fairId: number): Promise<void> {
-    await this.fairRepo.confirmDispatch(fairId);
+  async execute(fairId: number): Promise<Fair> {
+    return this.fairRepo.confirmDispatch(fairId);
   }
 }
 export {};
+
